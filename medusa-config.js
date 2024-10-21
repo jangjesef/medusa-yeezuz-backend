3571693002d5ce15784287a77c92c8b665a18403
Jan <jangajdosprod@gmail.com>
2024-10-21 15:52:24 +0200

    +

--- medusa-config.js
+++ medusa-config.js
@@ -1,21 +1,6 @@
 const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = "";
-switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
-}
+const ENV_FILE_NAME = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.dev';
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
@@ -27,10 +12,9 @@ const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://local
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+const DATABASE_URL = process.env.DATABASE_URL || "aws-0-eu-central-1.pooler.supabase.com";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "ample-turkey-33481.upstash.io";
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -57,25 +41,24 @@ const modules = {
   eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: process.env.REDIS_URL
     }
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: process.env.REDIS_URL
     }
   },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwt_secret: process.env.JWT_SECRET || "supersecret",
-  cookie_secret: process.env.COOKIE_SECRET || "supersecret",
+  jwt_secret: process.env.JWT_SECRET,
+  cookie_secret: process.env.COOKIE_SECRET,
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL,
   worker_mode: process.env.MEDUSA_WORKER_MODE,
 };
